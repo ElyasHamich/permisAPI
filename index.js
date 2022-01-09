c3787f52f87a1cf84fbb3592b43fc1c94949326d
ElyasHamich <elyas.hamich@student.ehb.be>
2022-01-09 03:58:02 +0100

    feat: add

--- index.js
+++ index.js
@@ -5,16 +5,9 @@ const seeder = require('./seeder');
 const app = express();
 
 
-seeder.questions.forEach(function(item, index, array) {
-    console.log(item, index);
-    console.log("------------------------------------------");
-});
-
-
-
 
 app.get('/', (req,res) => {
-    res.status(200).json(seeder.questions)
+    res.status(200).json(seeder)
 });
 
 app.get('/img')
@@ -28,5 +21,5 @@ app.use(express.static(__dirname + '/audio/N'));
 
 
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listenig on port ${port}`));
